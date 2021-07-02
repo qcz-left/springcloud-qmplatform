e0e11f6e17425f9e7d7fd590fabba8a5fc942af7
qcz_left <qcz_left@163.com>
2021-07-02 16:40:21 +0800

    update code

--- qmplatform-sms/src/main/java/com/qcz/qmplatform/sms/receiver/SmsReceiver.java
+++ qmplatform-sms/src/main/java/com/qcz/qmplatform/sms/receiver/SmsReceiver.java
@@ -3,15 +3,14 @@ package com.qcz.qmplatform.sms.receiver;
 import cn.hutool.core.bean.BeanUtil;
 import com.qcz.qmplatform.api.mq.RabbitConstant;
 import com.qcz.qmplatform.api.sms.dto.SmsDTO;
+import com.qcz.qmplatform.common.utils.SecureUtils;
 import com.qcz.qmplatform.sms.NotifyServiceFactory;
 import com.qcz.qmplatform.sms.bean.SmsConfig;
 import com.qcz.qmplatform.sms.utils.SmsUtils;
-import org.jasypt.encryption.StringEncryptor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.RabbitHandler;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -20,17 +19,13 @@ public class SmsReceiver {
 
     private static final Logger logger = LoggerFactory.getLogger(SmsReceiver.class);
 
-
-    @Autowired
-    private StringEncryptor encryptor;
-
     @RabbitHandler
     public void smsRabbitHandler(SmsDTO smsDTO) {
         logger.info("Receive sms queue information: {}", smsDTO);
         try {
             SmsConfig smsConfig = new SmsConfig();
             BeanUtil.copyProperties(smsDTO, smsConfig);
-            smsConfig.setSecretKey(encryptor.decrypt(smsConfig.getSecretKey()));
+            smsConfig.setSecretKey(SecureUtils.aesDecrypt(smsConfig.getSecretKey()));
 
             NotifyServiceFactory.build(SmsUtils.getNotifyServiceClass(smsDTO.getSmsProviderCode()), smsConfig).send();
         } catch (Exception e) {
